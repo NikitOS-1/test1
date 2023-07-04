b284e8424dab797148f47e3845995cf24613b898
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-04 18:45:41 +0300

    compl logic side bar

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,6 +1,5 @@
 import "./SideBar.scss";
 import logoIcon from "../../Assets/logo.png";
-
 import SideItem from "./SideItem/SideItem";
 
 const SideBar = () => {
@@ -12,11 +11,14 @@ const SideBar = () => {
           Dashboard<span>v.01</span>
         </h1>
       </header>
-      <SideItem icon={logoIcon} title="Product" />
-      <SideItem icon={logoIcon} title="Product" />
-      <SideItem icon={logoIcon} title="Product" />
-      <SideItem icon={logoIcon} title="Product" />
-      <SideItem icon={logoIcon} title="Product" />
+      <SideItem
+        icon={logoIcon}
+        title="Product"
+        item={[
+          { name: "Product", link: "#" },
+          { name: "Product", link: "#" },
+        ]}
+      />
     </div>
   );
 };
--- src/Components/SideBar/SideItem/SideItem.jsx
+++ src/Components/SideBar/SideItem/SideItem.jsx
@@ -1,28 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import arrowRight from "../../../Assets/arrowRight.png";
+import Item from "./Item/Item";
 
-const SideItem = ({ icon, title }) => {
-  const [currentStyleMenuItem, setCurrentStyleMenuItem] = useState(
-    "sidebar_hiden close"
-  );
+const SideItem = ({ icon, title, item }) => {
+  const [items, setItems] = useState(item);
+  const [isOpen, setIsOpen] = useState("close");
+
   const openMenu = () => {
-    setCurrentStyleMenuItem((prev) =>
-      prev === "sidebar_hiden open"
-        ? "sidebar_hiden close"
-        : "sidebar_hiden open"
-    );
+    setIsOpen((prev) => (prev === "open" ? "close" : "open"));
   };
+
   return (
     <div>
       <div className="sidebar_label" onClick={openMenu}>
         <img src={icon} alt="icon" />
         <span>{title}</span>
-        <img src={arrowRight} alt="arrow" />
+        <img src={arrowRight} alt="arrow" className={`arrow_` + `${isOpen}`} />
       </div>
-      <div className={currentStyleMenuItem}>
-        <div className="sidebar_hiden_menuItem">first</div>
-        <div className="sidebar_hiden_menuItem">first</div>
-        <div className="sidebar_hiden_menuItem">first</div>
+      <div className={`sidebar_hiden ` + `${isOpen}`}>
+        {items.map((i) => (
+          <div className="sidebar_hiden_menuItem" key={i.name}>
+            <span>
+              {i.name} link:{i.link}
+            </span>
+            {console.log(i)}
+          </div>
+        ))}
       </div>
     </div>
   );
