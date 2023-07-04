7f5dbd99ab6e4656f505bae48ee613610e206925
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-04 22:31:23 +0300

    add person

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,5 +1,6 @@
 import "./SideBar.scss";
 import SideItem from "./SideItem/SideItem";
+import Person from "../Person/Person";
 
 import logoIcon from "../../Assets/logo.svg";
 import dashboardIcon from "../../Assets/dashboard.svg";
@@ -12,58 +13,63 @@ import helpIcon from "../../Assets/help.svg";
 const SideBar = () => {
   return (
     <div className="sidebar">
-      <header>
-        <img src={logoIcon} alt="logo" />
-        <h1>
-          Dashboard<span>v.01</span>
-        </h1>
-      </header>
-      <div className="select-item">
-        <img src={dashboardIcon} alt="icon" />
-        <span>Dashboard</span>
+      <div>
+        <header>
+          <img src={logoIcon} alt="logo" />
+          <h1>
+            Dashboard<span>v.01</span>
+          </h1>
+        </header>
+        <div className="select-item">
+          <img src={dashboardIcon} alt="icon" />
+          <span>Dashboard</span>
+        </div>
+        <SideItem
+          icon={productIcon}
+          title="Product"
+          item={[
+            { name: "Category", link: "#" },
+            { name: "Favorite", link: "#" },
+            { name: "Sale", link: "#" },
+          ]}
+        />
+        <SideItem
+          icon={customersIcon}
+          title="Customers"
+          item={[
+            { name: "All Customers", link: "#" },
+            { name: "Active Members", link: "#" },
+          ]}
+        />
+        <SideItem
+          icon={incomeIcon}
+          title="Income"
+          item={[
+            { name: "First", link: "#" },
+            { name: "Second", link: "#" },
+          ]}
+        />
+        <SideItem
+          icon={promoteIcon}
+          title="Promote"
+          item={[
+            { name: "First", link: "#" },
+            { name: "Second", link: "#" },
+          ]}
+        />
+        <SideItem
+          icon={helpIcon}
+          title="Help"
+          item={[
+            { name: "Contact", link: "#" },
+            { name: "About", link: "#" },
+            { name: "Call Now", link: "#" },
+          ]}
+        />
       </div>
-      <SideItem
-        icon={productIcon}
-        title="Product"
-        item={[
-          { name: "Category", link: "#" },
-          { name: "Favorite", link: "#" },
-          { name: "Sale", link: "#" },
-        ]}
-      />
-      <SideItem
-        icon={customersIcon}
-        title="Customers"
-        item={[
-          { name: "All Customers", link: "#" },
-          { name: "Active Members", link: "#" },
-        ]}
-      />
-      <SideItem
-        icon={incomeIcon}
-        title="Income"
-        item={[
-          { name: "First", link: "#" },
-          { name: "Second", link: "#" },
-        ]}
-      />
-      <SideItem
-        icon={promoteIcon}
-        title="Promote"
-        item={[
-          { name: "First", link: "#" },
-          { name: "Second", link: "#" },
-        ]}
-      />
-      <SideItem
-        icon={helpIcon}
-        title="Help"
-        item={[
-          { name: "Contact", link: "#" },
-          { name: "About", link: "#" },
-          { name: "Call Now", link: "#" },
-        ]}
-      />
+      <div className="person">
+        <Person />
+      </div>
     </div>
   );
 };
