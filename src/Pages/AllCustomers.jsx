73561275f311b39d059f97ac135b8a9fbdcc7e73
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-05 19:06:44 +0300

    ref styles

--- src/Pages/AllCustomers.jsx
+++ src/Pages/AllCustomers.jsx
@@ -1,6 +1,8 @@
 import "./AllCustomers.scss";
 import Table from "../Components/Table/Table";
 import searchIcon from "../Assets/search.png";
+import { Pagination } from "@mui/material";
+
 const AllCustomers = () => {
   return (
     <div className="allCustomers">
@@ -15,6 +17,19 @@ const AllCustomers = () => {
         </div>
       </div>
       <Table />
+      <div className="wrapper_data_nav">
+        <div className="count_data">
+          <span>Showing data 1 to 8 of 256K entries</span>
+        </div>
+        <div className="pagination">
+          <Pagination
+            count={40}
+            variant="outlined"
+            shape="rounded"
+            color="secondary"
+          />
+        </div>
+      </div>
     </div>
   );
 };
