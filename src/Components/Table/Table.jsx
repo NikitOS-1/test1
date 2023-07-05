2c34c9fc2c8e5b0d8c24de07f1dad394e67f8c45
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-05 18:08:56 +0300

    ref

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,56 +1,49 @@
 import React from "react";
-import "./Table.css";
+import "./Table.scss";
 
 const Table = () => {
   const data = [
     {
-      id: 1,
-      name: "Item 1",
-      price: "$10",
-      stock: 15,
-      category: "Category A",
-      description: "Description for Item 1",
+      CustomerName: "Jane Cooper",
+      Company: "Microsoft",
+      PhoneNumber: "(225) 555-0118",
+      Email: "jane@microsoft.com",
+      Country: "United States",
+      Status: "Active",
     },
     {
-      id: 2,
-      name: "Item 2",
-      price: "$15",
-      stock: 8,
-      category: "Category B",
-      description: "Description for Item 2",
+      CustomerName: "Floyd Miles",
+      Company: "Yahoo",
+      PhoneNumber: "(205) 555-0100",
+      Email: "floyd@yahoo.com",
+      Country: "Kiribati",
+      Status: "Inactive",
     },
-    {
-      id: 3,
-      name: "Item 2",
-      price: "$15",
-      stock: 8,
-      category: "Category B",
-      description: "Description for Item 2",
-    },
-    // Добавьте другие элементы таблицы
   ];
 
   return (
-    <section>
-      <div className="grid-table">
-        <div className="grid-header">ID</div>
-        <div className="grid-header">Name</div>
-        <div className="grid-header">Price</div>
-        <div className="grid-header">Stock</div>
-        <div className="grid-header">Category</div>
-        <div className="grid-header">Description</div>
-        {data.map((item) => (
-          <React.Fragment key={item.id}>
-            <div className="grid-cell">{item.id}</div>
-            <div className="grid-cell">{item.name}</div>
-            <div className="grid-cell">{item.price}</div>
-            <div className="grid-cell">{item.stock}</div>
-            <div className="grid-cell">{item.category}</div>
-            <div className="grid-cell">{item.description}</div>
-          </React.Fragment>
-        ))}
+    <div className="grid-table">
+      <div className="wrap-grid-header">
+        <div className="grid-header">Customer Name</div>
+        <div className="grid-header">Company</div>
+        <div className="grid-header">Phone Number</div>
+        <div className="grid-header">Email</div>
+        <div className="grid-header">Country</div>
+        <div className="grid-header">Status</div>
       </div>
-    </section>
+      {data.map((item) => (
+        <React.Fragment key={item.id}>
+          <div className="wrap-grid-cell">
+            <div className="grid-cell">{item.CustomerName}</div>
+            <div className="grid-cell">{item.Company}</div>
+            <div className="grid-cell">{item.PhoneNumber}</div>
+            <div className="grid-cell">{item.Email}</div>
+            <div className="grid-cell">{item.Country}</div>
+            <div className="grid-cell">{item.Status}</div>
+          </div>
+        </React.Fragment>
+      ))}
+    </div>
   );
 };
 
--- src/Pages/AllCustomers.jsx
+++ src/Pages/AllCustomers.jsx
@@ -1,6 +1,21 @@
 import "./AllCustomers.scss";
-
+import Table from "../Components/Table/Table";
+import searchIcon from "../Assets/search.png";
 const AllCustomers = () => {
-  return <div className="allCustomers">AllCustomers</div>;
+  return (
+    <div className="allCustomers">
+      <div className="customer_wrapper">
+        <div className="title">
+          <p>All Customers</p>
+          <span>Active Members</span>
+        </div>
+        <div className="search">
+          <img src={searchIcon} alt="search" />
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+      <Table />
+    </div>
+  );
 };
 export default AllCustomers;
