d3df3d05261bf41f84b858860d40704ea0f0a157
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-07-05 16:23:20 +0300

    coml nav sections

--- src/Components/Person/Person.jsx
+++ src/Components/Person/Person.jsx
@@ -2,13 +2,13 @@ import "./Person.scss";
 
 const Person = ({ name, position, avatar }) => {
   return (
-    <div>
-      <div>
+    <div className="container">
+      <div className="avatar">
         <img src={avatar} alt="Avatar" />
       </div>
-      <div>
-        <h2>{name}</h2>
-        <h2>{position}</h2>
+      <div className="description">
+        <div className="name">{name}</div>
+        <div className="position">{position}</div>
       </div>
     </div>
   );
