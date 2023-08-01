54ab9ac5e4f22a0c868dd2ab60dd9cebe71239b4
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-08-01 20:58:34 +0300

    .

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,21 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./Table.scss";
 import Status from "./Status/Status";
-import axios from "axios";
 
-const BASE_URL = "http://hn.algolia.com/api/v1/search?";
-
-const Table = () => {
-  const [post, setPost] = useState([]);
-  const [query, setQuery] = useState("react");
-  const [page, setPage] = useState(1);
-  const [pageQty, setPageQty] = useState(0);
+const Table = ({ post }) => {
+  const [data, setData] = useState([]);
   useEffect(() => {
-    axios.get(BASE_URL + `query=${query}&page=${page - 1}`).then((data) => {
-      setPost(data.data.hits);
-      setPageQty(data.data.nbPages);
-    });
+    setData(post);
   }, []);
+  console.log(data);
   return (
     <div className="grid-table">
       <div className="wrap-grid-header">
@@ -26,18 +18,18 @@ const Table = () => {
         <div className="grid-header">Country</div>
         <div className="grid-header">Status</div>
       </div>
-      {/* {data.map((item) => (
-        <React.Fragment key={item.CustomerName}>
+      {data.map((item) => (
+        <React.Fragment>
           <div className="wrap-grid-cell">
-            <div className="grid-cell">{item.CustomerName}</div>
-            <div className="grid-cell">{item.Company}</div>
-            <div className="grid-cell">{item.PhoneNumber}</div>
-            <div className="grid-cell">{item.Email}</div>
-            <div className="grid-cell">{item.Country}</div>
-            <Status status={item.Status} />
+            <div className="grid-cell">{}</div>
+            <div className="grid-cell">{}</div>
+            <div className="grid-cell">{}</div>
+            <div className="grid-cell">{}</div>
+            <div className="grid-cell">{}</div>
+            <Status />
           </div>
         </React.Fragment>
-      ))} */}
+      ))}
     </div>
   );
 };
--- src/Pages/AllCustomers.jsx
+++ src/Pages/AllCustomers.jsx
@@ -2,8 +2,24 @@ import "./AllCustomers.scss";
 import Table from "../Components/Table/Table";
 import searchIcon from "../Assets/search.png";
 import { Pagination } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://hn.algolia.com/api/v1/search?";
 
 const AllCustomers = () => {
+  const [post, setPost] = useState([]);
+  const [query, setQuery] = useState("react");
+  const [page, setPage] = useState(1);
+  const [pageQty, setPageQty] = useState(0);
+
+  useEffect(() => {
+    axios.get(BASE_URL + `query=${query}&page=${page - 1}`).then((data) => {
+      setPost(data.data.hits);
+      setPageQty(data.data.nbPages);
+    });
+  }, [query, page]);
+
   return (
     <div className="allCustomers">
       <div className="customer_wrapper">
@@ -16,17 +32,19 @@ const AllCustomers = () => {
           <input type="text" placeholder="Search" />
         </div>
       </div>
-      <Table />
+      <Table post={post} />
       <div className="wrapper_data_nav">
         <div className="count_data">
           <span>Showing data 1 to 8 of 256K entries</span>
         </div>
         <div className="pagination">
           <Pagination
-            count={40}
+            count={pageQty}
+            page={page}
             variant="outlined"
             shape="rounded"
             color="secondary"
+            onChange={(_, num) => setPage(num)}
             style={{ width: "max-content" }}
           />
         </div>
